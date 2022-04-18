1d85c34e42c4b8108ec93f83579bc43b1408fcd2
Riya Kansal <78086124+riyakansal14@users.noreply.github.com>
2022-04-18 21:50:41 +0530

    Update Sort_an_array_of_0s,_1s_and_2s.cpp

--- Day_16/Sort_an_array_of_0s,_1s_and_2s.cpp
+++ Day_16/Sort_an_array_of_0s,_1s_and_2s.cpp
@@ -1,20 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
 class Solution
 {
     public:
     void sort012(int a[], int n)
     {
-        int i,j;
-        //code here
-        for(i=1;i<n;i++)
+        int l,m,h;
+        l=0;
+        m=0;
+        h=n-1;
+        while(m<=h)
         {
-            int cur = a[i];
-            j = i-1;
-            while(a[j]>cur){
-                a[j+1]=a[j];
-                j--;
+            if(a[m] == 0)
+            {
+                swap(a[l],a[m]);
+                m++;
+                l++;
             }
-            a[j+1]=cur;
+            else if(a[m] == 1)
+                m++;
+            else if(a[m] == 2)
+            {
+                swap(a[m],a[h]);
+                h--;
+            }
         }
     }
     
 };
+
+// { Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+        }
+
+        Solution ob;
+        ob.sort012(a, n);
+
+        for(int i=0;i<n;i++){
+            cout << a[i]  << " ";
+        }
+
+        cout << endl;
+        
+        
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
